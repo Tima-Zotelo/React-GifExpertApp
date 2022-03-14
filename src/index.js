887ab3d7e3ad7caf90cd90c4c9eb80474f82e7ed
Matias Zotelo <timacjsntvg@gmail.com>
2022-03-14 12:43:45 -0300

    html nuevamente actualizado

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import GifExpertApp from './GifExpertApp';
 import './index.css';
 
-const divRoot = document.getElementById('root');
+const divRoot = document.querySelector('#root');
 
 ReactDOM.render(<GifExpertApp />, divRoot);
 
